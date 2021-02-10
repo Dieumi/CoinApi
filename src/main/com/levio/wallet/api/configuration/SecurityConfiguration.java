b7608f078abd08edc61383e4be8bb5ca9f65b0d0
Pierre <oxeyo@hotmail.fr>
2021-02-10 17:57:58 -0500

    updated jar and security

--- src/main/com/levio/wallet/api/configuration/SecurityConfiguration.java
+++ src/main/com/levio/wallet/api/configuration/SecurityConfiguration.java
@@ -60,7 +60,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().antMatchers("/authenticate").permitAll().
+                .authorizeRequests().antMatchers("/").permitAll()
+                .antMatchers("/authenticate").permitAll().
                  antMatchers("/registerUser").hasAuthority("ADMIN").
                     anyRequest().authenticated().and().
                     exceptionHandling().and().sessionManagement()
